4ed1c6eace4552d39febdd329d76f30502323988
Markus Ullmark <ullmark@gmail.com>
2012-03-28 11:13:20 +0200

    added package build on release build

--- Clippy/Properties/AssemblyInfo.cs
+++ Clippy/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("24HR.Extensions")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("Clippy")]
+[assembly: AssemblyDescription("Clippy contains a set of extensions to make you more productive")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("24HR.Extensions")]
+[assembly: AssemblyCompany("24HR")]
+[assembly: AssemblyProduct("Clippy")]
 [assembly: AssemblyCopyright("Copyright ©  2012")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("0.1.0.0")]
+[assembly: AssemblyFileVersion("0.1.0.0")]
