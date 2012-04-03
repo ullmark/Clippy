e3a795a82202c4fa36615643b405049601058870
Markus Ullmark <ullmark@gmail.com>
2012-04-03 12:24:21 +0200

    crated a valueextensions class

---
+++ Clippy/Enumerable/ValueExtensions.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+public static class ValueExtensions
+{
+    public static bool IsNotNullAndHaveItems<T>(this IEnumerable<T> collection)
+    {
+        if (collection == null)
+            return false;
+        return collection.Any();
+    }
+}
