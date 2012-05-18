c54bdf5192f5eaca3d4d5f398e544d45817b0e61
Markus Ullmark <ullmark@gmail.com>
2012-05-18 11:04:08 +0200

    added a clear method to memory cache

---
+++ Clippy.Test/Caching/MemoryCacheExtensions/Clear.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Xunit;
+using System.Runtime.Caching;
+using FluentAssertions;
+
+namespace Clippy.Test.Caching.MemoryCacheExtensions
+{
+	public class Clear
+	{
+		[Fact]
+		public void It_removes_all_items()
+		{
+			MemoryCache.Default.Add("Hello", "Again", DateTimeOffset.Now.AddHours(1));
+			MemoryCache.Default.Select(x => x.Key).Count().Should().Be(1);
+			MemoryCache.Default.Clear();
+			MemoryCache.Default.Select(x => x.Key).Count().Should().Be(0);
+		}
+	}
+}
---
+++ Clippy/Caching/MemoryCacheExtensions.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Runtime.Caching;
+
+public static class MemoryCacheExtensions
+{
+	public static void Clear(this MemoryCache cache)
+	{
+		var keys = cache.Select(x => x.Key).ToList();
+		keys.Each(x => cache.Remove(x));
+	}
+}
