847fb974bbb17160c00804c3c9049e3ca5ddd266
Markus Ullmark <ullmark@gmail.com>
2012-05-31 10:50:53 +0200

    created a "jsonorjsonpresult"

---
+++ Clippy.Mvc/ActionResults/JsonOrJsonpResult.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Web.Mvc;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Serialization;
+
+namespace Clippy.Mvc.ActionResults
+{
+	public class JsonOrJsonpResult : ActionResult
+	{
+		public override void ExecuteResult(ControllerContext context)
+		{
+			var request = context.HttpContext.Request;
+			var response = context.HttpContext.Response;
+			response.ContentType = "application/json";
+
+			var callback = request.Params["callback"];
+			if (!string.IsNullOrWhiteSpace(callback))
+			{
+				response.ContentType = "application/javascript";
+				response.Write(string.Concat(callback, "("));
+			}
+
+			if (this.Data != null)
+			{
+				// Create a SettingsObject
+				var jsonSerializerSettings = new JsonSerializerSettings
+				{
+					NullValueHandling = NullValueHandling.Ignore,
+					ContractResolver = new CamelCasePropertyNamesContractResolver()
+				};
+
+				var json = JsonConvert.SerializeObject(this.Data, Formatting.None, jsonSerializerSettings);
+				response.Write(json);
+			}
+
+			if (!string.IsNullOrWhiteSpace(callback))
+			{
+				response.Write(");");
+			}	
+		}
+
+		public virtual object Data { get; set; }
+	}
+}
---
+++ Clippy.Test/ActionResults/JsonOrJsonpResultTests.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Xunit;
+using Moq;
+using System.Web.Mvc;
+using System.Web;
+using System.Collections.Specialized;
+using Clippy.Mvc.ActionResults;
+using FluentAssertions;
+
+namespace Clippy.Test.ActionResults
+{
+	public class JsonOrJsonpResultTests
+	{
+		private Mock<ControllerContext> controllerContext;
+		private Mock<HttpRequestBase> request;
+		private Mock<HttpResponseBase> response;
+		private NameValueCollection param;
+		private StringBuilder content;
+		private string contentType;
+
+		public JsonOrJsonpResultTests()
+		{
+			content = new StringBuilder();
+			param = new NameValueCollection();
+			controllerContext = new Mock<ControllerContext>();
+			request = new Mock<HttpRequestBase>();
+			response = new Mock<HttpResponseBase>();
+
+			request.SetupGet(x => x.Params).Returns(param);
+			response.Setup(x => x.Write(It.IsAny<string>()))
+				.Callback((string s) => content.Append(s));
+
+			response.SetupSet(x => x.ContentType = It.IsAny<string>()).Callback((string s) => contentType = s);
+
+			controllerContext.SetupGet(x => x.HttpContext.Request).Returns(request.Object);
+			controllerContext.SetupGet(x => x.HttpContext.Response).Returns(response.Object);
+		}
+
+		[Fact]
+		public void It_renders_json_correctly()
+		{
+			var result = new JsonOrJsonpResult { Data = new { foo = "bar" } };
+			result.ExecuteResult(controllerContext.Object);
+
+			content.ToString().Should().Be(
+				@"{""foo"":""bar""}");
+
+			contentType.Should().Be("application/json");
+		}
+
+		[Fact]
+		public void It_renders_jsonp_correctly()
+		{
+			param["callback"] = "func";
+			var result = new JsonOrJsonpResult { Data = new { foo = "bar" } };
+			result.ExecuteResult(controllerContext.Object);
+			
+			content.ToString().Should().Be(
+				@"func({""foo"":""bar""});");
+
+			contentType.Should().Be("application/javascript");
+		}
+	}
+}
