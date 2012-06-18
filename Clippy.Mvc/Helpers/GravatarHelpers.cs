6f36f74e0aca939b65c536c7667956ec92bf6877
Petter Klang <petter@24hr.se>
2012-06-18 14:09:18 +0200

    Added so you can ask for other default icons

--- Clippy.Mvc/Helpers/GravatarHelpers.cs
+++ Clippy.Mvc/Helpers/GravatarHelpers.cs
@@ -7,39 +7,45 @@ using System.Security.Cryptography;
 
 public static class GravatarHelpers
 {
-	private const string gravatar = "http://www.gravatar.com/avatar/";
-	
-	public static MvcHtmlString Gravatar(this HtmlHelper helper, string email, string fallback = null, int? size = null)
-	{
-		var img = new TagBuilder("img");
-		img.Attributes["alt"] = string.Empty;
-		
-		var url = string.Concat(gravatar, ConstructGravatarUrl(email));
-		if (size.HasValue)
-		{
-			if (size.Value < 1 || size.Value > 512)
-				throw new ArgumentOutOfRangeException("size", "allowed values are 1 - 512");
+    private const string gravatar = "http://www.gravatar.com/avatar/";
 
-			url = url.AddQueryStringParameter("s", size.Value.ToString());
-		}
+    public static MvcHtmlString Gravatar(this HtmlHelper helper, string email, string fallback = null, int? size = null, string defaultIconKeyWord = null)
+    {
+        var img = new TagBuilder("img");
+        img.Attributes["alt"] = string.Empty;
 
-		img.Attributes["src"] = url; 
-		return MvcHtmlString.Create(img.ToString(TagRenderMode.SelfClosing));
-	}
+        var myGravatar = gravatar;
 
-	private static string ConstructGravatarUrl(string email)
-	{
-		var md5Hasher = MD5.Create();
-		var utf8 = new UTF8Encoding();
+        var url = string.Concat(gravatar, ConstructGravatarUrl(email));
 
-		var emailHash = md5Hasher.ComputeHash(utf8.GetBytes(email));
-        var s = new StringBuilder();
+        if (!string.IsNullOrEmpty(defaultIconKeyWord))
+            url = url.AddQueryStringParameter("d", defaultIconKeyWord);
 
-        // Loop through each byte of the hashed data 
-        // and format each one as a hexadecimal string.
-        for (int i = 0; i < emailHash.Length; i++)
-            s.Append(emailHash[i].ToString("x2"));
+        if (size.HasValue)
+        {
+            if (size.Value < 1 || size.Value > 512)
+                throw new ArgumentOutOfRangeException("size", "allowed values are 1 - 512");
 
-        return s.ToString();
-	}
+            url = url.AddQueryStringParameter("s", size.Value.ToString());
+        }
+
+        img.Attributes["src"] = url;
+        return MvcHtmlString.Create(img.ToString(TagRenderMode.SelfClosing));
+    }
+
+    private static string ConstructGravatarUrl(string email)
+    {
+        var md5Hasher = MD5.Create();
+        var utf8 = new UTF8Encoding();
+
+        var emailHash = md5Hasher.ComputeHash(utf8.GetBytes(email));
+        var s = new StringBuilder();
+
+        // Loop through each byte of the hashed data 
+        // and format each one as a hexadecimal string.
+        for (int i = 0; i < emailHash.Length; i++)
+            s.Append(emailHash[i].ToString("x2"));
+
+        return s.ToString();
+    }
 }
--- Clippy.Test/Mvc/Helpers/GravatarHelpers/Gravatar.cs
+++ Clippy.Test/Mvc/Helpers/GravatarHelpers/Gravatar.cs
@@ -8,24 +8,33 @@ using System.Web.Mvc;
 
 namespace Clippy.Test.Mvc.Helpers.GravatarHelpers
 {
-	public class Gravatar : AHtmlHelperTest
-	{
-		[Fact]
-		public void It_produces_the_correct_markup()
-		{
-			helper.Gravatar("example@example.com")
-				.ToString()
-				.Should()
-				.Be(@"<img alt="""" src=""http://www.gravatar.com/avatar/23463b99b62a72f26ed677cc556c44e8"" />");
-		}
+    public class Gravatar : AHtmlHelperTest
+    {
+        [Fact]
+        public void It_produces_the_correct_markup()
+        {
+            helper.Gravatar("example@example.com")
+                .ToString()
+                .Should()
+                .Be(@"<img alt="""" src=""http://www.gravatar.com/avatar/23463b99b62a72f26ed677cc556c44e8"" />");
+        }
 
-		[Fact]
-		public void It_works_with_size()
-		{
-			helper.Gravatar("example@example.com",  size: 50)
-				.ToString()
-				.Should()
-				.Be(@"<img alt="""" src=""http://www.gravatar.com/avatar/23463b99b62a72f26ed677cc556c44e8?s=50"" />");
-		}
-	}
+        [Fact]
+        public void It_works_with_size()
+        {
+            helper.Gravatar("example@example.com", size: 50)
+                .ToString()
+                .Should()
+                .Be(@"<img alt="""" src=""http://www.gravatar.com/avatar/23463b99b62a72f26ed677cc556c44e8?s=50"" />");
+        }
+
+        [Fact]
+        public void It_works_with_defaulticonkeyword()
+        {
+            helper.Gravatar("example@example.com", null, null, "dd")
+                .ToString()
+                .Should()
+                .Be(@"<img alt="""" src=""http://www.gravatar.com/avatar/23463b99b62a72f26ed677cc556c44e8?d=dd"" />");
+        }
+    }
 }
