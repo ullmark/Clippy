7bf19fe5c15658dbdb127cb6cd3ddf429e93c239
Markus Ullmark <ullmark@gmail.com>
2012-07-16 09:25:33 +0200

    created a *random* extension on ienumerable

---
+++ Clippy.Test/Enumerable/UtilExtensions/Random.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using FluentAssertions;
+using Xunit;
+
+namespace Clippy.Test.Enumerable.UtilExtensions
+{
+	public class Random
+	{
+		[Fact]
+		public void Random_picks_a_element()
+		{
+			for (var i = 0; i < 50; i++) 
+			{
+				var values = new []{ 5, 3, 2, 1, 0, 1000, 20 };
+				var value = values.Random();
+				values.Should().Contain(value);
+			}
+		}
+	}
+}
---
+++ Clippy/Enumerable/UtilExtensions.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+public static class UtilExtensions
+{
+	public static T Random<T>(this IEnumerable<T> collection)
+	{
+		if (collection == null)
+			throw new ArgumentNullException("collection");
+
+		var count = collection.Count();
+		
+		if (count == 0)
+			return default(T);
+
+		var random = new Random();
+		var next = random.Next(count);
+
+		return collection.ElementAt(next);
+	}
+}
