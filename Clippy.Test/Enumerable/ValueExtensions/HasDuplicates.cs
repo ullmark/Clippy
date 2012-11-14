15c7ca632695ae05f048b36e4c329d53c24437db
Kristian Laustsen <kristian@laustsen.se>
2012-11-14 09:08:02 +0100

    Add enumerable extension for checking duplicates

---
+++ Clippy.Test/Enumerable/ValueExtensions/HasDuplicates.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using FluentAssertions;
+using Xunit;
+
+namespace Clippy.Test.Enumerable.ValueExtensions
+{
+    public class HasDuplicates
+    {
+        [Fact]
+        public void It_returns_true_if_enumerable_has_duplicates()
+        {
+            var items = new List<string>() {
+                "a",
+                "b",
+                "c",
+                "c"
+            };
+
+            items.HasDuplicates().Should().BeTrue();
+        }
+
+        [Fact]
+        public void It_returns_false_if_enumerable_does_not_have_duplicates()
+        {
+            var items = new List<string>() {
+                "a",
+                "b",
+                "c",
+                "d"
+            };
+
+            items.HasDuplicates().Should().BeFalse();
+        }
+
+        [Fact]
+        public void It_throws_an_exception_if_enumerable_is_null()
+        {
+            IEnumerable<string> items = null;
+
+            Action call = () => items.HasDuplicates();
+            call.ShouldThrow<ArgumentNullException>();
+        }
+
+        [Fact]
+        public void It_throws_an_exception_if_comparer_is_null()
+        {
+            var items = new List<string>() {
+                "a",
+                "b",
+                "c",
+                "d"
+            };
+
+            Action call = () => items.HasDuplicates(null);
+            call.ShouldThrow<ArgumentNullException>();
+        }
+    }
+}
--- Clippy/Enumerable/ValueExtensions.cs
+++ Clippy/Enumerable/ValueExtensions.cs
@@ -11,4 +11,39 @@ public static class ValueExtensions
             return false;
         return collection.Any();
     }
+
+    /// <summary>
+    /// Checks if the Enumerable contains duplicates
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    /// <param name="subjects"></param>
+    /// <returns>True if the Enumerable contains duplicates</returns>
+    public static bool HasDuplicates<T>(this IEnumerable<T> subjects)
+    {
+        return HasDuplicates(subjects, EqualityComparer<T>.Default);
+    }
+
+    /// <summary>
+    /// Checks if the Enumerable contains duplicates
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    /// <param name="subjects"></param>
+    /// <param name="comparer"></param>
+    /// <returns>True if the Enumerable contains duplicates</returns>
+    public static bool HasDuplicates<T>(this IEnumerable<T> subjects, IEqualityComparer<T> comparer)
+    {
+        if (subjects == null)
+            throw new ArgumentNullException("subjects");
+
+        if (comparer == null)
+            throw new ArgumentNullException("comparer");
+
+        var set = new HashSet<T>(comparer);
+
+        foreach (var s in subjects)
+            if (!set.Add(s))
+                return true;
+
+        return false;
+    }
 }
